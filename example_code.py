bbd3930f4765156d75d7ee65753bf79abe03621e
user <reutapel88@gmail.com>
2016-05-03 18:58:17 +0300

    Merge branch 'master' of https://github.com/reutapel/e-commerce-models
    # Conflicts:
    #	.idea/workspace.xml

--- example_code.py
+++ example_code.py
@@ -1,67 +1,38 @@
 import csv
 import math
 import random
+from sklearn.cross_validation import KFold
 
-Product_customer_rank ={}
-#{(P_i,C_j):rank , (P_m,C_n):rank , .....}
-
-Customr_product_rank = {}
-#{(C_i,P_j):rank , (C_m,P_n):rank , .....}
-
-customer_product_list = []
-#[(C_i,P_j), (C_m,P_n), .....]
-dfdf
-product_neighbors = {}
-#{product1:[product1_neighbor1 , product1_neighbor2, ...] , product2:[product2_neighbor1 , product2_neighbor2, ...], ... }
-
-########################################################################################
-########################################################################################
-################### read P_C_matrix into dictionaries ####################################
-with open("P_C_matrix.csv","r") as csvfile:
-    reader = csv.DictReader(csvfile)
-    # remember : field_names = ['Product_ID', 'Customer_ID','Customer_rank' ]
-    for row in reader:
-        Product_customer_rank[(row['Product_ID'],row['Customer_ID'])] = int(row['Customer_rank'])
-        Customr_product_rank[(row['Customer_ID'],row['Product_ID'])] = int(row['Customer_rank'])
-        customer_product_list.append((row['Customer_ID'],row['Product_ID']))
-csvfile.close()
-########################################################git stat###############################
-#######################################################################################       
-        
-########################################################################################
-########################################################################################
-####### another view of P_C_matrix #####################################################
-customer_product_list.sort()
-with open('C_P_matrix.csv', 'w' ) as csvfile:
-    writer = csv.writer(csvfile, lineterminator='\n')
-    fieldnames = ["Customer_ID", "Proudct_ID","Customer_rank"]
-    writer.writerow(fieldnames)
-
-    for C_P in customer_product_list:
-        writer.writerow([  C_P[0] , C_P[1], Customr_product_rank[C_P] ])
-csvfile.close()
-#######################################################################################
-#######################################################################################
+#cross validation function:
+# 1. split the training set to train and validation sets.
+# 2. run the model on the train set
+# 3. validate the model on the validation set
+def CrossValidation (Customr_product_rank):
+    kf = KFold(len(Customr_product_rank), 3, shuffle=False)
+    for train, test in kf:
+        print 'Start run the model'
+        Product_customer_train, Product_customer_test = [Customr_product_rank.keys()[i] for i in train], [Customr_product_rank.keys()[i] for i in test]
+        Rank_train, Rank_test = [Customr_product_rank.itervalues()[i] for i in train], [Customr_product_rank.itervalues()[i] for i in test]
+        R_avg_train = AvgDicValues(Rank_train)
+        Bu, Bi = minimizeRMSE_model(Product_customer_train, Rank_train, R_avg_train)
+        estimated_ranks = estimatedRanks(Product_customer_test, R_avg_train, Bu, Bi)
+        RMSE = evaluatModel(Product_customer_test, Rank_test,estimated_ranks, test)
+        print 'The RMSE of the model on this test set is: {}'. format(RMSE)
 
 
-########################################################################################
-################### read Network_arcs into dictionaries ################################
-#################  Remember : Network_arcs is a directed graph #########################
-with open("Network_arcs.csv","r") as csvfile2:
-    reader2 = csv.DictReader(csvfile2)
-    # remember : field_names = ['Product1_ID', 'Product2_ID']
-    for row in reader2:
-        if row['Product1_ID'] in product_neighbors:
-            product_neighbors[row['Product1_ID']].append(row['Product2_ID'])
-        else:
-            product_neighbors[row['Product1_ID']] = [row['Product2_ID']]
-csvfile2.close()
-#######################################################################################
-#######################################################################################
+# Calculate for each customer and for each product the Bu and Bi
+def minimizeRMSE_model (Product_customer , Ranks, R_avg):
+    return
 
 
+# Calculate for each Product_customer couple the estimated value for the rank
+def estimatedRanks (Product_customer_test, R_avg, Bu, Bi):
+    return
 
 
+# Evaluate the model: calculate the RMSE for the test set
+def evaluateModel (Product_customer_test , Ranks_test, estimated_ranks):
+    return sum(pow((estimated_ranks[i] - Ranks_test[i] for i in test),2)) #check if that works
 
 
 # average of dictionary values- for calculating the average rank
@@ -91,7 +62,7 @@ def RelCusPro():
 
 # random.random() returns float between 0.0 to 1.0
 # returns test and training dictionaries with key: product, customer and value: rank
-def KfoldDevision(dict, threshold ):
+def KfoldDevision(dict, threshold):
     traindic = {}
     testdic = {}
     for key, value in dict:
@@ -102,50 +73,96 @@ def KfoldDevision(dict, threshold ):
     return traindic, testdic
 
 
+def main():
+    # type: () -> object
+    Product_customer_rank ={}
+    #{(P_i,C_j):rank , (P_m,C_n):rank , .....}
+
+    Customr_product_rank = {}
+    #{(C_i,P_j):rank , (C_m,P_n):rank , .....}
+
+    customer_product_list = []
+    #[(C_i,P_j), (C_m,P_n), .....]
+
+    product_neighbors = {}
+    #{product1:[product1_neighbor1 , product1_neighbor2, ...] , product2:[product2_neighbor1 , product2_neighbor2, ...], ... }
 
 
+########################################################################################
+########################################################################################
+################### read P_C_matrix into dictionaries ####################################
+    with open("P_C_matrix.csv","r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        # remember : field_names = ['Product_ID', 'Customer_ID','Customer_rank' ]
+        for row in reader:
+            Product_customer_rank[(row['Product_ID'],row['Customer_ID'])] = int(row['Customer_rank'])
+            Customr_product_rank[(row['Customer_ID'],row['Product_ID'])] = int(row['Customer_rank'])
+            customer_product_list.append((row['Customer_ID'],row['Product_ID']))
+    csvfile.close()
+#######################################################################################
+#######################################################################################
+
+########################################################################################
+########################################################################################
+####### another view of P_C_matrix #####################################################
+    customer_product_list.sort()
+    with open('C_P_matrix.csv', 'w' ) as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n')
+        fieldnames = ["Customer_ID", "Product_ID","Customer_rank"]
+        writer.writerow(fieldnames)
+
+        for C_P in customer_product_list:
+            writer.writerow([  C_P[0] , C_P[1], Customr_product_rank[C_P] ])
+    csvfile.close()
+#######################################################################################
+#######################################################################################
 
 
-
-
-
-
-
-
-
-
-
+########################################################################################
+################### read Network_arcs into dictionaries ################################
+#################  Remember : Network_arcs is a directed graph #########################
+    with open("Network_arcs.csv","r") as csvfile2:
+        reader2 = csv.DictReader(csvfile2)
+        # remember : field_names = ['Product1_ID', 'Product2_ID']
+        for row in reader2:
+            if row['Product1_ID'] in product_neighbors:
+                product_neighbors[row['Product1_ID']].append(row['Product2_ID'])
+            else:
+                product_neighbors[row['Product1_ID']] = [row['Product2_ID']]
+    csvfile2.close()
+#######################################################################################
+#######################################################################################
 
 ########################################################################################
 ########################################################################################
 ############################       results        ######################################
-product_last_rank = {}
-for P_C in Product_customer_rank:
-    product_last_rank[P_C[0]] = int(Product_customer_rank[P_C])
-    
-results_list = []
-# remember : field_names = ['Product_ID', 'Customer_ID','Customer_rank' ]
-with open("results.csv","r") as read_file:
-    reader3 = csv.DictReader(read_file)
-    for row in reader3:
-        if row['Product_ID'] in product_last_rank:
-            results_list.append( (row['Product_ID'] ,row['Customer_ID'] , product_last_rank[( row['Product_ID'] )]  )  )
-        else:
-            results_list.append( (row['Product_ID'] ,row['Customer_ID'] , 5  )  )
-read_file.close()
+    product_last_rank = {}
+    for P_C in Product_customer_rank:
+        product_last_rank[P_C[0]] = int(Product_customer_rank[P_C])
+
+    results_list = []
+    # remember : field_names = ['Product_ID', 'Customer_ID','Customer_rank' ]
+    with open("results.csv","r") as read_file:
+        reader3 = csv.DictReader(read_file)
+        for row in reader3:
+            if row['Product_ID'] in product_last_rank:
+                results_list.append( (row['Product_ID'] ,row['Customer_ID'] , product_last_rank[( row['Product_ID'] )]  )  )
+            else:
+                results_list.append( (row['Product_ID'] ,row['Customer_ID'] , 5  )  )
+    read_file.close()
 
 ########################################################################################
 ########################################################################################
-#######  output file ###################################################################        
-with open('EX2.csv', 'w' ) as write_file:e
-    writer = csv.writer(write_file, lineterminator='\n')
-    fieldnames2 = ["Proudct_ID" , "Customer_ID" ,"Customer_rank"]
-    writer.writerow(fieldnames2)
-    for result in results_list:
-        writer.writerow([  result[0] , result[1] , int(result[2])  ])
-write_file.close
+#######  output file ###################################################################
+    with open('EX2.csv', 'w' ) as write_file:
+        writer = csv.writer(write_file, lineterminator='\n')
+        fieldnames2 = ["Proudct_ID" , "Customer_ID" ,"Customer_rank"]
+        writer.writerow(fieldnames2)
+        for result in results_list:
+            writer.writerow([  result[0] , result[1] , int(result[2])  ])
+        write_file.close
 #######################################################################################
 #######################################################################################
 
-y=1
-        
+if __name__ == '__main__':
+    main()
